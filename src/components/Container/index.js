88b1ceab5d20788d4c36dbc86e830525cacbeaea
Adam Welnicki <adam.welnicki@gmail.com>
2020-03-16 13:02:58 +0100

    003 Added scss partials - reset, variables

--- src/components/Container/index.js
+++ src/components/Container/index.js
@@ -1,10 +1,14 @@
 import React from "react";
 import './style/style.scss'
+import '../../stylePartials/_reset.scss'
+import '../../stylePartials/_variables.scss'
 
-const Container = () => {
+const Container = (props) => {
     return (
         <div className={'window'}>
-
+            <div className={'container'}>
+                {props.children}
+            </div>
         </div>
     )
 };
