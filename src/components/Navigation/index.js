f76b76d2fb3918c148b265a3fefaa8aae4615efb
Adam Welnicki <adam.welnicki@gmail.com>
2020-03-16 16:44:28 +0100

    004 Navigation bar and logo added

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,8 +1,23 @@
 import React from "react";
+import './style/style.scss'
 
 const Navigation = () => {
     return (
-        null
+        <nav>
+            <h1 className={'logo'}>VV<span>app</span></h1>
+            <i className="fas fa-bars"></i>
+            <ul>
+                <li>
+                    <a href={'#'}>Szukaj</a>
+                </li>
+                <li>
+                    <a href={'#'}>O<span className={'list_logo1'}>VV</span>
+                        <span className={'list_logo2'}>app</span></a>
+                </li>
+                <li><a href={'#'}>Jak używać</a></li>
+                <li><a href={'#'}>Kontakt</a></li>
+            </ul>
+        </nav>
     )
 };
 
