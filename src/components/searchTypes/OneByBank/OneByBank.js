cf8f6a94f3939db88503d894bacade257a7406be
Adam Welnicki <adam.welnicki@gmail.com>
2020-03-18 23:01:58 +0100

    015 OneByBank Component validation finished, installed final-form-focus

--- src/components/searchTypes/OneByBank/OneByBank.js
+++ src/components/searchTypes/OneByBank/OneByBank.js
@@ -1,8 +1,6 @@
-import React, {useEffect, useState} from "react";
+import React, {useState} from "react";
 import {Form, Field} from 'react-final-form';
 import './oneByBank.scss';
-
-const bankAccount = '94814522512683789765158332';
 
 // Current date
 const date = new Date();
@@ -13,17 +11,32 @@ let exactDate = `${date.getFullYear()}-${date.getMonth() < 9 ? '0' +
 const OneByBank = ({typeReturn}) => {
     const [error, setError] = useState(false);
     const [ownDate, setOwnDate] = useState(false);
+    const [userDate, setUserDate] = useState(exactDate);
+    const [bankAccount, setBankAccount] = useState("");
 
     // Own date checkbox check
     const handleCheck = () => {
         setOwnDate(!ownDate);
+        if (userDate > exactDate && ownDate === true) {
+            setUserDate(exactDate);
+        }
     };
 
     const hiddenClass = ownDate ? "" : "hideData";
 
+    // saving userDate in useState
+    const userDateHandler = (event) => {
+        setUserDate(event.target.value)
+    };
+
+    // saving value of bankAccount input in useState
+    const bankAccHandler = (event) => {
+        setBankAccount(event.target.value)
+    };
+
     // Server GET on Form submit
     const onSubmit = () => {
-        fetch(`https://wl-test.mf.gov.pl/api/search/bank-account/${bankAccount}?date=${exactDate}`)
+        fetch(`https://wl-test.mf.gov.pl/api/search/bank-account/${bankAccount}?date=${userDate}`)
             .then((res) => {
                 setError(false);
                 return res.json();
@@ -33,7 +46,27 @@ const OneByBank = ({typeReturn}) => {
             });
     };
 
-    const required = (value) => (value ? undefined : "Wymagane");
+    // Requirements for the input bankAccount
+    const required = (value) => {
+        if (value.length === 0) {
+            return "wymagane";
+        } else if (value.length < 26 || isNaN(value)) {
+            return "Podaj poprawny numer konta"
+        } else {
+            return undefined;
+        }
+    };
+
+    // Requirements for the user Date
+    const requiredData = (value) => {
+        console.log(ownDate);
+        if (value > exactDate && ownDate === true) {
+            return "Data nie może być datą przyszłą";
+        } else {
+            return undefined;
+        }
+    };
+
 
     return (
         <section className={'searchOption'}>
@@ -43,34 +76,52 @@ const OneByBank = ({typeReturn}) => {
             </div>
             {error === true &&
             <span className={'server_error'}>Coś poszło nie tak. Nie można odnaleźć servera.</span>}
-            <Form onSubmit={onSubmit}>
+            <Form onSubmit={onSubmit} subscription={{submitting: true}}>
                 {({handleSubmit, submitting, values}) => (
                     <form onSubmit={handleSubmit}>
-                        <div className={'bankAcc'}>
-                            <label className={'bankAccL'}>Numer konta bankowego:</label>
-                            <Field id={'bankAccNr'} name={'bankAccNr'}
-                                   component={"input"}
-                                   placeholder={'Numer konta bankowego'}
-                            />
-                        </div>
-                        <p>Data: <span>{exactDate}</span></p>
+
+                        <Field name={'bankAccNr'}
+                               placeholder={'Numer konta bankowego'}
+                               validate={required}
+                               defaultValue={bankAccount}>
+                            {({input, meta, placeholder}) => (
+                                <div className={`bankAcc ${meta.active ? 'activeInput' : ""}`}>
+                                    <label className={'bankAccL'}>Numer konta bankowego:</label>
+                                    <input {...input}
+                                           placeholder={placeholder}
+                                           id={'bankAccNr'}
+                                           maxLength={26}
+                                           onChange={bankAccHandler}/>
+                                    {meta.error && meta.touched &&
+                                    <span className={'metaError'}>{meta.error}</span>}
+                                </div>
+                            )}
+                        </Field>
+
+                        <p>Data: <span>{ownDate ? userDate : exactDate}</span></p>
                         <div className={'ownDateDiv'}>
                             <label>Podaj własną datę</label>
                             <Field className={'ownDateCheckbox'} name={'ownDate'}
                                    component={"input"}
                                    type={'checkbox'}
                                    checked={ownDate}
-                                   onChange={handleCheck}
-                            />
+                                   onChange={handleCheck}/>
                         </div>
                         <div className={hiddenClass}>
-                            <div>
-                                <label>Data:</label>
-                                <Field id={'userDate'} name={'userDate'}
-                                       component={"input"}
-                                       type={'date'}
-                                />
-                            </div>
+                            <Field name={'userDate'}
+                                   component={"input"}
+                                   type={'date'}
+                                   defaultValue={userDate}
+                                   validate={requiredData}>
+                                {({input, meta}) => (
+                                    <div>
+                                        <label>Data:</label>
+                                        <input {...input}
+                                               id={'userDate'}
+                                               onChange={userDateHandler}/>
+                                        {meta.error && meta.invalid &&
+                                        <span className={'metaError'}>{meta.error}</span>}
+                                    </div>)}</Field>
                         </div>
                         <button type="submit" disabled={submitting}>Wyślij</button>
                         <pre>{JSON.stringify(values, undefined, 2)}</pre>
